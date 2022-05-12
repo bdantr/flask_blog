8b056a683f3877e364134d1e43532144dd6e4297
bdant <bogdansapr@gmail.com>
2022-05-12 17:11:07 +0300

    adding db with users tables

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'try-to-guess'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ models.py
@@ -0,0 +1,8 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(64), unique=True,index=True)
+    email = db.Column(db.String(120), unique=True, index=True)
+    password = db.Column(db.String(256))
+
