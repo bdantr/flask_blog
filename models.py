c3af3615ecaa628f23aec3eea4dba6b7654162cb
100BallovLego <bogdansapr@gmail.com>
2022-05-16 17:50:41 +0300

    adding user loader, password generating

--- models.py
+++ models.py
@@ -1,12 +1,22 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(64), unique=True,index=True)
     email = db.Column(db.String(120), unique=True, index=True)
     password = db.Column(db.String(256))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 
 class Post(db.Model):
