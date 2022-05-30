6b77720989b7bfa9816763202723c89b7fe58ba4
100Ballov <bogdansapr@gmail.com>
2022-05-30 18:00:51 +0300

    adding profile

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user, login_required
-from app import app, login
+from app import app, login, db
 from forms import LoginForm,RegistrationForm
 from models import User
 
@@ -21,8 +21,8 @@ def login():
         return redirect(url_for('main_page'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data)
-        if user is None or not User.check_password(form.password.data):
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('main_page'))
@@ -40,6 +40,15 @@ def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(
+            username=form.username.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
 
@@ -47,3 +56,11 @@ def register():
 @login_required
 def support():
     return render_template('support.html')
+
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html',user=user)
